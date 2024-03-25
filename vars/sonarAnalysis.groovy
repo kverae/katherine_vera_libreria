c56e71b9df6c4af863e2874754151ca329a40277
Katherine Vera <katherine.vera94@gmail.com>
2024-03-25 15:30:52 -0400

    se agrega variable de entorno para la rama y espera de 5 segundos

--- vars/sonarAnalysis.groovy
+++ vars/sonarAnalysis.groovy
@@ -1,25 +1,38 @@
 def call(boolean abortOnFailure = false, boolean abortPipeline = false) {
-    // Ejecutar el escaneo de SonarQube (o un mensaje de prueba)
-    sh 'echo "Ejecución de las pruebas de calidad de código"'
-    
-    // Esperar 5 minutos con un timeout para el resultado
-    def qualityGateStatus = waitForQualityGate()
-    
-    // Evaluar el resultado del QualityGate y abortar el pipeline si es necesario
-    if (abortOnFailure && qualityGateStatus != 'OK') {
-        error "Quality Gate failure detected. Aborting pipeline."
-    }
-    
-    // Abortar el pipeline si se indica
+    // Verificar si se debe abortar siempre según el parámetro
     if (abortPipeline) {
         error "Pipeline aborted by user request."
+    }
+    
+    // Leer el nombre de la rama de Git de la variable de entorno
+    def gitBranch = env.BRANCH_NAME
+    
+    // Determinar si se debe cortar el pipeline según la heurística
+    if (abortOnFailure || gitBranch == 'master' || gitBranch.startsWith('hotfix')) {
+        error "Pipeline aborted due to Quality Gate failure or branch name: ${gitBranch}."
+    } else {
+        // Ejecutar el escaneo de SonarQube (o un mensaje de prueba)
+        sh 'echo "Ejecución de las pruebas de calidad de código"'
+        
+        // Esperar 5 minutos con un timeout para el resultado
+        def qualityGateStatus = waitForQualityGate()
+        
+        // Evaluar el resultado del QualityGate y abortar el pipeline si es necesario
+        if (qualityGateStatus != 'OK') {
+            error "Quality Gate failure detected. Aborting pipeline."
+        }
     }
 }
 
 def waitForQualityGate() {
-    // Simular la espera de 5 minutos
-    // En una implementación real, aquí se llamaría a SonarQube API para verificar el estado del Quality Gate
+    // Esperar 5 minutos con un timeout
+    def qualityGateStatus = timeout(time: 5, unit: 'SECONDS') {
+        // Simular la espera de verificación del Quality Gate
+        // En una implementación real, aquí se llamaría a SonarQube API para verificar el estado del Quality Gate
+        
+        // Suponiendo que el estado del Quality Gate es OK
+        return 'OK'
+    }
     
-    // Suponiendo que el estado del Quality Gate es OK
-    return 'OK'
+    return qualityGateStatus
 }
